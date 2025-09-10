ca625bb05bf2244445d47b52d2fcf4ee2cc2533f
Sebastián Ortiz <sortiz7841@uta.edu.ec>
2025-09-09 23:03:45 -0500

    Se agregó la carpeta Services y sus archivos necesarios

---
+++ MiApiRest/MiApiRest/Services/IProductoService.cs
@@ -0,0 +1,13 @@
+using APIREST_WINDOWS_.Models;
+
+namespace APIREST_WINDOWS_.Services
+{
+    public interface IProductoService
+    {
+        IEnumerable<Producto> ObtenerProductos();
+        Producto? ObtenerProducto(int id);
+        void CrearProducto(Producto producto);
+        void ActualizarProducto(Producto producto);
+        void EliminarProducto(int id);
+   }
+}
